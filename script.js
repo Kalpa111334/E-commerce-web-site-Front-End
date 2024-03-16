c7b563c81c30924526dfeb33a4c2a8b7b3c37674
Kalpa Wishvajith Sirikumara <122417297+Kalpa111334@users.noreply.github.com>
2024-03-16 12:02:37 +0530

    Third Commit
    My E-commerce web Application Front-End

---
+++ script.js
@@ -0,0 +1,36 @@
+function manageu() {
+    window.location.href = "manageusers.html";
+}
+
+function managep() {
+    window.location.href = "manageproduct.html";
+}
+
+function w() {
+    window.location.href = "watchlist.html";
+
+
+}
+
+function c() {
+
+    window.location.href = "cart.html";
+
+}
+
+function iPhone() {
+
+    window.location.href = "singleProductView.html";
+
+}
+
+function p() {
+
+    window.location.href = "purchasehistory.html";
+
+}
+
+
+
+
+
